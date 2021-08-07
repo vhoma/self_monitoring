642f422900e26cadb4b12a3edcae52828b462f59
Boris Kefer <boriskefer@Boriss-MacBook-Air.local>
2021-08-08 01:00:27 +0300

    change again

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def respond():
     else:
         try:
             # clear the message we got from any non alphabets
-            text = re.sub(r"\W", "_", text)
+            # text = re.sub(r"\W", "_", text)
             # # create the api link for the avatar based on http://avatars.adorable.io/
             # url = "https://api.adorable.io/avatars/285/{}.png".format(text.strip())
             # # reply with a photo to the name the user sent,
